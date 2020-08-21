2775a2c1b59b38369485475c358f49ebf9d85e52
Shobhit <Shobhit@192.168.0.105>
2020-08-21 17:36:43 +0530

    changes done

---
+++ src/test/java/com/tests/IRCTC/Test2.java
@@ -0,0 +1,13 @@
+package com.tests.IRCTC;
+
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+
+	@Test
+	public void shania()
+	{
+		System.out.println("new Test class is created");
+	}
+}
