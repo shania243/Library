cb402987ac65547078db5367a2676042b973690c
Shobhit <Shobhit@192.168.0.105>
2020-08-21 17:32:05 +0530

    TestNew class has been added.

---
+++ src/test/java/com/tests/IRCTC/TestNew.java
@@ -0,0 +1,13 @@
+package com.tests.IRCTC;
+
+import org.testng.annotations.Test;
+
+public class TestNew 
+{
+
+	@Test
+	public void newTest()
+	{
+		System.out.println("Test class is created");
+	}
+}
